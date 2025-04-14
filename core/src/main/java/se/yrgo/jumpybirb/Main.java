6a9d486f4ab06d731c1f9b4fdde053520050d8aa
MarioGit24 <marbug0222@skola.goteborg.se>
2025-04-14 08:55:04 +0200

    Lagt till kod i obstacles  och i main

--- core/src/main/java/se/yrgo/jumpybirb/Main.java
+++ core/src/main/java/se/yrgo/jumpybirb/Main.java
@@ -1,11 +1,5 @@
 package se.yrgo.jumpybirb;
 
-import java.math.BigDecimal;
-import java.nio.BufferOverflowException;
-
-import javax.swing.text.FieldView;
-import javax.swing.text.Position;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -16,31 +10,45 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all
+ * platforms.
+ */
 public class Main implements ApplicationListener {
 
-    Texture backgroundTexture; 
+    private static final int OBSTACLE_SPACING = 40;
+    private static final int OBSTACLE_COUNT = 3;
+
+    Texture backgroundTexture;
     FitViewport viewport;
-    SpriteBatch spriteBatch; 
-    
-    Texture birbTexture; 
+    SpriteBatch spriteBatch;
+
+    Texture birbTexture;
     Sprite birbSprite;
-    Birb birb; 
-    
+    Birb birb;
+
+    Obstacles[] obstacles;
 
     @Override
     public void create() {
-        
-        birbTexture = new Texture("assets\\bird.png");
+
+        birbTexture = new Texture("bird.png");
         birbSprite = new Sprite(birbTexture);
         birbSprite.setSize(10, 10);
         birbSprite.setCenterY(25);
         birbSprite.setCenterX(25);
         birb = new Birb(birbTexture, birbSprite);
-        backgroundTexture = new Texture("assets\\background.jpg");
+
+        backgroundTexture = new Texture("background.jpg");
         viewport = new FitViewport(75, 50);
-        spriteBatch = new SpriteBatch(); 
-        
+        spriteBatch = new SpriteBatch();
+
+        obstacles = new Obstacles[OBSTACLE_COUNT];
+        for (int i = 0; i < OBSTACLE_COUNT; i++) {
+            float x = 60 + i * (OBSTACLE_SPACING + Obstacles.TUBE_WIDTH);
+            obstacles[i] = new Obstacles(x);
+        }
+
     }
 
     @Override
@@ -52,52 +60,59 @@ public class Main implements ApplicationListener {
     @Override
     public void render() {
         input();
+        logic();
         draw();
-        logic(); 
     }
 
-    public void draw(){
-        ScreenUtils.clear(Color.BLACK); 
+    public void draw() {
+        ScreenUtils.clear(Color.BLACK);
         viewport.apply();
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
         spriteBatch.begin();
 
-
-        
         float width = viewport.getWorldWidth();
         float height = viewport.getWorldHeight();
-     
-       
 
         spriteBatch.draw(backgroundTexture, 0, 0, width, height);
         birbSprite.draw(spriteBatch);
-        //birbSprite.
-        
 
-       
-   
-        
+        for (Obstacles obs : obstacles) {
+            System.out.println(obs.getPosTopTube());
+            spriteBatch.draw(obs.getTopTube(), obs.getPosTopTube().x, obs.getPosTopTube().y);
+            spriteBatch.draw(obs.getBottomTube(), obs.getPosBottomTube().x, obs.getPosBottomTube().y);
+        }
 
         spriteBatch.end();
 
-
     }
 
-    public void input(){
+    public void input() {
         float jumpHeight = 100f;
         float delta = Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)){
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             birbSprite.translateY(jumpHeight * delta);
 
-
-        }else{
+        } else {
             birbSprite.translateY(-8f * delta);
 
         }
 
     }
 
-    public void logic(){
+    public void logic() {
+        for (Obstacles obs : obstacles) {
+            obs.getPosTopTube().x -= 20 * Gdx.graphics.getDeltaTime();
+            obs.getPosBottomTube().x -= 20 * Gdx.graphics.getDeltaTime();
+
+            if (obs.getPosTopTube().x + Obstacles.TUBE_WIDTH < 0) {
+                obs.reposition(viewport.getWorldWidth() + Obstacles.TUBE_WIDTH);
+            }
+
+            if (obs.collides(birbSprite.getBoundingRectangle())) {
+                System.out.println("\uD83D\uDCA5 Kollision! Game Over");
+                // TODO: Stoppa spelet eller visa meny
+            }
+        }
 
     }
 
@@ -113,6 +128,11 @@ public class Main implements ApplicationListener {
 
     @Override
     public void dispose() {
-        // Destroy application's resources here.
+        backgroundTexture.dispose();
+        birbTexture.dispose();
+        spriteBatch.dispose();
+        for (Obstacles obs : obstacles) {
+            obs.dispose();
+        }
     }
 }
--- core/src/main/java/se/yrgo/jumpybirb/Obstacles.java
+++ core/src/main/java/se/yrgo/jumpybirb/Obstacles.java
@@ -1,5 +1,64 @@
 package se.yrgo.jumpybirb;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
 public class Obstacles {
-    
+
+    public static final int TUBE_WIDTH = 80;
+    private static final int FLUCTUATION = 130;
+    private static final int TUBE_GAP = 150;
+    private static final int LOWEST_OPENING = 100;
+
+    private Texture topTube, bottomTube;
+    private Vector2 posTopTube, posBottomTube;
+    private Rectangle boundsTop, boundsBottom;
+
+    public Obstacles(float x) {
+        topTube = new Texture("toptube.png");
+        bottomTube = new Texture("bottomtube.png");
+
+        float offset = (float)(Math.random() * FLUCTUATION);
+
+        posTopTube = new Vector2(x, offset + TUBE_GAP + LOWEST_OPENING);
+        posBottomTube = new Vector2(x, offset + LOWEST_OPENING);
+
+        boundsTop = new Rectangle(posTopTube.x, posTopTube.y, TUBE_WIDTH, topTube.getHeight());
+        boundsBottom = new Rectangle(posBottomTube.x, posBottomTube.y, TUBE_WIDTH, bottomTube.getHeight());
+    }
+
+    public void reposition(float x) {
+        float offset = (float) (Math.random() * FLUCTUATION);
+        posTopTube.set(x, offset + TUBE_GAP + LOWEST_OPENING);
+        posBottomTube.set(x, offset + LOWEST_OPENING);
+
+        boundsTop.setPosition(posTopTube.x, posTopTube.y);
+        boundsBottom.setPosition(posBottomTube.x, posBottomTube.y);
+    }
+
+    public boolean collides(Rectangle player) {
+        return player.overlaps(boundsTop) || player.overlaps(boundsBottom);
+    }
+
+    public Texture getTopTube() {
+        return topTube;
+    }
+
+    public Texture getBottomTube() {
+        return bottomTube;
+    }
+
+    public Vector2 getPosTopTube() {
+        return posTopTube;
+    }
+
+    public Vector2 getPosBottomTube() {
+        return posBottomTube;
+    }
+
+    public void dispose() {
+        topTube.dispose();
+        bottomTube.dispose();
+    }
 }
